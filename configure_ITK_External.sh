5fea92bfdee31a242ef50b66888bd98918d73caa
ncullen93 <ncullen.th@dartmouth.edu>
2017-09-23 00:49:49 -0400

    cache

--- configure_ITK_External.sh
+++ configure_ITK_External.sh
@@ -25,49 +25,46 @@ itktag=c5138560409c75408ff76bccff938f21e5dcafc6 #4.12
 # remove it
 
 # if no directory, clone ITK in `itksource` dir
-if [[ ! -d itksource ]]; then 
-    git clone $itkgit itksource;
-    cd itksource;
-    git checkout master;
-    git checkout $itktag;
-    cd ../
-fi
+git clone $itkgit itksource;
+cd itksource;
+git checkout master;
+git checkout $itktag;
+cd ../
 
-if [[ ! -d itkbuild ]]; then
-    mkdir -p itkbuild
-    cd itkbuild
-    compflags=" -fPIC -O2  "
-    cmake \
-        -DCMAKE_BUILD_TYPE:STRING="${CMAKE_BUILD_TYPE}" \
-        -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -Wno-c++11-long-long -fPIC -O2 -DNDEBUG  "\
-        -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -Wno-c++11-long-long -fPIC -O2 -DNDEBUG  "\
-        -DITK_USE_GIT_PROTOCOL:BOOL=OFF \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DBUILD_TESTING:BOOL=OFF \
-        -DBUILD_EXAMPLES:BOOL=OFF \
-        -DCMAKE_INSTALL_PREFIX:PATH=${R_PACKAGE_DIR}/libs/  \
-        -DITK_LEGACY_REMOVE:BOOL=OFF  \
-        -DITK_FUTURE_LEGACY_REMOVE:=BOOL=ON \
-        -DITKV3_COMPATIBILITY:BOOL=ON \
-        -DITK_BUILD_DEFAULT_MODULES:BOOL=OFF \
-        -DKWSYS_USE_MD5:BOOL=ON \
-        -DITK_WRAPPING:BOOL=OFF \
-        -DModule_MGHIO:BOOL=ON \
-        -DModule_ITKDeprecated:BOOL=OFF \
-        -DModule_ITKReview:BOOL=ON \
-        -DModule_ITKVtkGlue:BOOL=OFF \
-        -D ITKGroup_Core=ON \
-        -D Module_ITKReview=ON \
-        -D ITKGroup_Filtering=ON \
-        -D ITKGroup_IO=ON \
-        -D ITKGroup_Numerics=ON \
-        -D ITKGroup_Registration=ON \
-        -D ITKGroup_Segmentation=ON \
-        -DCMAKE_C_VISIBILITY_PRESET:BOOL=hidden \
-        -DCMAKE_CXX_VISIBILITY_PRESET:BOOL=hidden \
-        -DCMAKE_VISIBILITY_INLINES_HIDDEN:BOOL=ON ../itksource/
-    make -j 3
-    #make install
-    cd ../
-fi
+mkdir -p itkbuild
+cd itkbuild
+compflags=" -fPIC -O2  "
+cmake \
+    -DCMAKE_BUILD_TYPE:STRING="${CMAKE_BUILD_TYPE}" \
+    -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -Wno-c++11-long-long -fPIC -O2 -DNDEBUG  "\
+    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -Wno-c++11-long-long -fPIC -O2 -DNDEBUG  "\
+    -DITK_USE_GIT_PROTOCOL:BOOL=OFF \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DBUILD_TESTING:BOOL=OFF \
+    -DBUILD_EXAMPLES:BOOL=OFF \
+    -DCMAKE_INSTALL_PREFIX:PATH=${R_PACKAGE_DIR}/libs/  \
+    -DITK_LEGACY_REMOVE:BOOL=OFF  \
+    -DITK_FUTURE_LEGACY_REMOVE:=BOOL=ON \
+    -DITKV3_COMPATIBILITY:BOOL=ON \
+    -DITK_BUILD_DEFAULT_MODULES:BOOL=OFF \
+    -DKWSYS_USE_MD5:BOOL=ON \
+    -DITK_WRAPPING:BOOL=OFF \
+    -DModule_MGHIO:BOOL=ON \
+    -DModule_ITKDeprecated:BOOL=OFF \
+    -DModule_ITKReview:BOOL=ON \
+    -DModule_ITKVtkGlue:BOOL=OFF \
+    -D ITKGroup_Core=ON \
+    -D Module_ITKReview=ON \
+    -D ITKGroup_Filtering=ON \
+    -D ITKGroup_IO=ON \
+    -D ITKGroup_Numerics=ON \
+    -D ITKGroup_Registration=ON \
+    -D ITKGroup_Segmentation=ON \
+    -DCMAKE_C_VISIBILITY_PRESET:BOOL=hidden \
+    -DCMAKE_CXX_VISIBILITY_PRESET:BOOL=hidden \
+    -DCMAKE_VISIBILITY_INLINES_HIDDEN:BOOL=ON ../itksource/
+make -j 3
+#make install
+cd ../
+
 
