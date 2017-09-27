05f20e5f9860500b5a7974c06ef5ece08745f73d
ncullen93 <ncullen.th@dartmouth.edu>
2017-09-27 11:22:36 -0400

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -138,32 +138,6 @@ htmlhelp_basename = 'ANTsPydoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-#latex_documents = [
-#    (master_doc, 'pytorch.tex', 'PyTorch Documentation',
-#     'Torch Contributors', 'manual'),
-#]
-latex_documents = []
 
 
 # -- Options for manual page output ---------------------------------------
