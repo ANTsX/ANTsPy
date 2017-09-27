ea50a1b9ba2cdbe83f69d8df31d2238579584083
ncullen93 <ncullen.th@dartmouth.edu>
2017-09-27 12:03:19 -0400

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,10 @@ import os
 import sys
 import shutil
 
-# copy source code into docs directory
+# add package to local path
 sys.path.insert(0, os.path.abspath('../../'))
 autodoc_mock_imports = ['_tkinter', 'matplotlib']
+# replace lib __init__ with empty init file since RTD cant handle C++ extensions
 os.remove('../../ants/lib/__init__.py')
 shutil.move('emptyinit.py', '../../ants/lib/__init__.py')
 
