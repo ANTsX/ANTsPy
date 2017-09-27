c2bb2a34943a0acdb72c67bff0eed2723db90277
ncullen93 <ncullen.th@dartmouth.edu>
2017-09-27 16:17:13 -0400

    init

--- ants/lib/__init__.py
+++ ants/lib/__init__.py
@@ -50,9 +50,3 @@ from .TileImages import *
 #from .NonLocalSuperResolution import *
 #from .SuperResolution import *
 #from .TimeSCCAN import *
-
-
-## VIZ ##
-from .antsSurf import *
-from .antsVol import *
-from .ConvertScalarImageToRGB import *
