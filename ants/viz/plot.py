c7de6e6b77832ddf172d76fd31e09fb2423560f2
ncullen93 <ncullen.th@dartmouth.edu>
2017-10-29 21:40:59 -0400

    movie plots

--- ants/viz/plot.py
+++ ants/viz/plot.py
@@ -9,7 +9,7 @@ TODO:
 """
 
 
-__all__ = ['plot',
+__all__ = ['plot', 'movie',
            'plot_hist',
            'plot_grid',
            'plot_ortho', 'plot_ortho_double', 'plot_ortho_stack',
@@ -26,6 +26,7 @@ import matplotlib.patheffects as path_effects
 import matplotlib.lines as mlines
 import matplotlib.patches as patches
 import matplotlib.mlab as mlab
+import matplotlib.animation as animation
 
 import numpy as np
 
@@ -34,6 +35,58 @@ from ..core import ants_image as iio
 from ..core import ants_image_io as iio2
 from ..core import ants_transform as tio
 from ..core import ants_transform_io as tio2
+
+
+def movie(image, filename=None, writer=None, fps=30):
+    """
+    Create and save a movie - mp4, gif, etc - of the various
+    2D slices of a 3D ants image
+    
+    Try this:
+        conda install -c conda-forge ffmpeg
+
+    Example
+    -------
+    >>> import ants
+    >>> mni = ants.image_read(ants.get_data('mni'))
+    >>> ants.movie(mni, filename='~/desktop/movie.mp4')
+    """
+
+    image = image.pad_image()
+    img_arr = image.numpy()
+
+    minidx = max(0, np.where(image > 0)[0][0] - 5)
+    maxidx = max(image.shape[0], np.where(image > 0)[0][-1] + 5)
+
+    # Creare your figure and axes
+    fig, ax = plt.subplots(1)
+
+    im = ax.imshow(img_arr[minidx,:,:], animated=True, 
+        cmap='Greys_r', vmin=image.quantile(0.05), vmax=image.quantile(0.95))
+
+    ax.axis('off')
+
+    def init():
+        fig.axes('off')
+        return im,
+
+    def updatefig(frame):
+        im.set_array(img_arr[frame,:,:])
+        return im,
+
+    ani = animation.FuncAnimation(fig, updatefig, frames=np.arange(minidx,maxidx),
+                                  #init_func=init, 
+                                  interval=50, blit=True)
+
+    if writer is None:
+        writer = animation.FFMpegWriter(fps=fps)
+
+    if filename is not None:
+        filename = os.path.expanduser(filename)
+        ani.save(filename, writer=writer)
+    else:
+        plt.show()
+
 
 
 def plot_hist(image, threshold=0., fit_line=False, normfreq=True,
