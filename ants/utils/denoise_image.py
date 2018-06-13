d41f1e732f00d0e64c0407ee0be6036f533eb0b7
stnava <stnava@gmail.com>
2018-06-13 08:06:01 -0400

    PERF: allow no mask in denoise image

--- ants/utils/denoise_image.py
+++ ants/utils/denoise_image.py
@@ -1,5 +1,5 @@
 
- 
+
 
 __all__ = ['denoise_image']
 
@@ -7,11 +7,11 @@ from .. import utils
 from . import process_args as pargs
 from .get_mask import get_mask
 
-def denoise_image(image, mask=None, shrink_factor=1, p=1, r=3, noise_model='Rician'):
+def denoise_image(image, mask=None, shrink_factor=1, p=1, r=3, noise_model='Rician', v=0 ):
     """
-    Denoise an image using a spatially adaptive filter originally described in 
-    J. V. Manjon, P. Coupe, Luis Marti-Bonmati, D. L. Collins, and M. Robles. 
-    Adaptive Non-Local Means Denoising of MR Images With Spatially Varying 
+    Denoise an image using a spatially adaptive filter originally described in
+    J. V. Manjon, P. Coupe, Luis Marti-Bonmati, D. L. Collins, and M. Robles.
+    Adaptive Non-Local Means Denoising of MR Images With Spatially Varying
     Noise Levels, Journal of Magnetic Resonance Imaging, 31:192-203, June 2010.
 
     ANTsR function: `denoiseImage`
@@ -20,22 +20,22 @@ def denoise_image(image, mask=None, shrink_factor=1, p=1, r=3, noise_model='Rici
     ---------
     image : ANTsImage
         scalar image to denoise.
-    
+
     mask : ANTsImage
         to limit the denoise region.
-    
-    shrink_factor : scalar   
+
+    shrink_factor : scalar
         downsampling level performed within the algorithm.
-    
+
     p : integer
         patch radius for local sample.
-    
+
     r : integer
         search radius from which to choose extra local samples.
-    
+
     noise_model : string
         'Rician' or 'Gaussian'
-    
+
     Returns
     -------
     ANTsImage
@@ -52,24 +52,33 @@ def denoise_image(image, mask=None, shrink_factor=1, p=1, r=3, noise_model='Rici
     inpixeltype = image.pixeltype
     outimage = image.clone('float')
 
-    if mask is None:
-        mask = get_mask(image)
-
     mydim = image.dimension
-    myargs = {
-        'd': mydim,
-        'i': image,
-        'n': noise_model,
-        'x': mask.clone('unsigned char'),
-        's': int(shrink_factor),
-        'p': p,
-        'r': r,
-        'o': outimage,
-        'v': 0
-    }
+
+    if mask is None:
+        myargs = {
+            'd': mydim,
+            'i': image,
+            'n': noise_model,
+            's': int(shrink_factor),
+            'p': p,
+            'r': r,
+            'o': outimage,
+            'v': v
+        }
+    else :
+        myargs = {
+            'd': mydim,
+            'i': image,
+            'n': noise_model,
+            'x': mask.clone('unsigned char'),
+            's': int(shrink_factor),
+            'p': p,
+            'r': r,
+            'o': outimage,
+            'v': v
+        }
+
     processed_args = pargs._int_antsProcessArguments(myargs)
     libfn = utils.get_lib_fn('DenoiseImage')
     libfn(processed_args)
     return outimage.clone(inpixeltype)
-
-
