0eecd6230d564e18d11c1d5be248951679e1d202
stnava <stnava@gmail.com>
2018-11-01 16:24:48 -0400

    all tests

--- tests/run_tests_travis.sh
+++ tests/run_tests_travis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-PYCMD=${PYCMD:="python"}
+PYCMD=${PYCMD:="python3"}
 COVERAGE=0
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
@@ -27,20 +27,20 @@ $PYCMD test_core_ants_transform.py $@
 $PYCMD test_core_ants_transform_io.py $@
 $PYCMD test_core_ants_metric.py $@
 
-# echo "Running learn tests"
-# $PYCMD test_learn.py $@
+echo "Running learn tests"
+$PYCMD test_learn.py $@
 
-# echo "Running registation tests"
-# $PYCMD test_registation.py $@
+echo "Running registation tests"
+$PYCMD test_registation.py $@
 
-# echo "Running segmentation tests"
-# $PYCMD test_segmentation.py $@
+echo "Running segmentation tests"
+$PYCMD test_segmentation.py $@
 
-# echo "Running utils tests"
-# $PYCMD test_utils.py $@
+echo "Running utils tests"
+$PYCMD test_utils.py $@
 
-# echo "Running bug tests"
-# $PYCMD test_bugs.py $@
+echo "Running bug tests"
+$PYCMD test_bugs.py $@
 
 
 
