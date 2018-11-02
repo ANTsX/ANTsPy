13307e8395b19b23a0dc577f389d94543ed40755
brian avants <stnava@gmail.com>
2018-11-02 15:43:01 -0400

    BUG: In kmeans

--- ants/segmentation/kmeans.py
+++ ants/segmentation/kmeans.py
@@ -1,5 +1,5 @@
 
- 
+
 
 __all__ = ['kmeans_segmentation']
 
@@ -11,18 +11,18 @@ def kmeans_segmentation(image, k, kmask=None, mrf=0.1):
     K-means image segmentation that is a wrapper around `ants.atropos`
 
     ANTsR function: `kmeansSegmentation`
-    
+
     Arguments
     ---------
-    image : ANTsImage 
+    image : ANTsImage
         input image
-    
+
     k : integer
         integer number of classes
-    
-    kmask : ANTsImage (optional)   
+
+    kmask : ANTsImage (optional)
         segment inside this mask
-    
+
     mrf : scalar
         smoothness, higher is smoother
 
@@ -41,11 +41,9 @@ def kmeans_segmentation(image, k, kmask=None, mrf=0.1):
     kmimage = utils.iMath(image, 'Normalize')
     if kmask is None:
         kmask = utils.get_mask(kmimage, 0.01, 1, cleanup=2)
-    kmask = utils.iMath(kmask, 'FillHoles')
+    kmask = utils.iMath(kmask, 'FillHoles').threshold_image(1,2)
     nhood = 'x'.join(['1']*dim)
     mrf = '[%s,%s]' % (str(mrf), nhood)
     kmimage = atropos(a = kmimage, m = mrf, c = '[5,0]', i = 'kmeans[%s]'%(str(k)), x = kmask)
     kmimage['segmentation'] = kmimage['segmentation'].clone(image.pixeltype)
     return kmimage
-
-
