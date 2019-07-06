30b003882f18e15fc55c3818bee4f6981a9aad2d
stnava <stnava@gmail.com>
2019-07-06 17:29:36 -0400

    BUG: mat mul not *

--- ants/utils/convert_nibabel.py
+++ ants/utils/convert_nibabel.py
@@ -1,5 +1,4 @@
-
-__all__ = ['to_nibabel', 'from_nibabel']
+__all__ = ["to_nibabel", "from_nibabel"]
 
 import os
 from tempfile import mktemp
@@ -12,13 +11,19 @@ def to_nibabel(image):
     Convert an ANTsImage to a Nibabel image
     """
     if image.dimension != 3:
-        raise ValueError('Only 3D images currently supported')
+        raise ValueError("Only 3D images currently supported")
 
     import nibabel as nib
+
     array_data = image.numpy()
-    affine = np.hstack([image.direction*np.diag(image.spacing),np.array(image.origin).reshape(3,1)])
-    affine = np.vstack([affine, np.array([0,0,0,1.])])
-    affine[:2,:] *= -1
+    affine = np.hstack(
+        [
+            np.matmul(image.direction, np.diag(image.spacing)),
+            np.array(image.origin).reshape(3, 1),
+        ]
+    )
+    affine = np.vstack([affine, np.array([0, 0, 0, 1.0])])
+    affine[:2, :] *= -1
     new_img = nib.Nifti1Image(array_data, affine)
     return new_img
 
@@ -27,7 +32,7 @@ def from_nibabel(nib_image):
     """
     Convert a nibabel image to an ANTsImage
     """
-    tmpfile = mktemp(suffix='.nii.gz')
+    tmpfile = mktemp(suffix=".nii.gz")
     nib_image.to_filename(tmpfile)
     new_img = iio2.image_read(tmpfile)
     os.remove(tmpfile)
