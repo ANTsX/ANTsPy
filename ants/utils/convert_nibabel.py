804696126480f36fddd74a8a0d24b0c4274f0f54
Daniel Gomez <d.gomez@posteo.org>
2019-07-16 16:51:27 -0400

    Use safe mkstemp instead of unsafe mktemp in format conversion

--- ants/utils/convert_nibabel.py
+++ ants/utils/convert_nibabel.py
@@ -1,7 +1,7 @@
 __all__ = ["to_nibabel", "from_nibabel"]
 
 import os
-from tempfile import mktemp
+from tempfile import mkstemp
 import numpy as np
 from ..core import ants_image_io as iio2
 
@@ -11,9 +11,10 @@ def to_nibabel(image):
     Convert an ANTsImage to a Nibabel image
     """
     import nibabel as nib
-    tmpfile = mktemp(suffix=".nii.gz")
+    fd, tmpfile = mkstemp(suffix=".nii.gz")
     image.to_filename(tmpfile)
     new_img = nib.load(tmpfile)
+    os.close(fd)
     # os.remove(tmpfile) ## Don't remove tmpfile as nibabel lazy loads the data.
     return new_img
 
@@ -22,8 +23,9 @@ def from_nibabel(nib_image):
     """
     Convert a nibabel image to an ANTsImage
     """
-    tmpfile = mktemp(suffix=".nii.gz")
+    fd, tmpfile = mkstemp(suffix=".nii.gz")
     nib_image.to_filename(tmpfile)
     new_img = iio2.image_read(tmpfile)
+    os.close(fd)
     os.remove(tmpfile)
     return new_img
