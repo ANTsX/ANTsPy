7366ca3a18c0a294653ac459f8ec13255e3ac039
stnava <stnava@gmail.com>
2019-07-30 09:38:39 -0400

    WIP: test to_ants function

--- ants/utils/convert_nibabel.py
+++ ants/utils/convert_nibabel.py
@@ -1,4 +1,4 @@
-__all__ = ["to_nibabel", "from_nibabel"]
+__all__ = ["to_nibabel", "from_nibabel", "to_ants"]
 
 import os
 from tempfile import mkstemp
@@ -11,6 +11,7 @@ def to_nibabel(image):
     Convert an ANTsImage to a Nibabel image
     """
     import nibabel as nib
+
     fd, tmpfile = mkstemp(suffix=".nii.gz")
     image.to_filename(tmpfile)
     new_img = nib.load(tmpfile)
@@ -29,3 +30,35 @@ def from_nibabel(nib_image):
     os.close(fd)
     os.remove(tmpfile)
     return new_img
+
+
+def to_ants(img: nib.Nifti1Image):
+    """
+    Converts a given Nifti image into an ANTsPy image
+
+    Parameters
+    ----------
+        img: NiftiImage
+
+    Returns
+    -------
+        ants_image: ANTsImage
+    """
+    ndim = img.ndim
+    q_form = img.get_qform()
+    spacing = img.header["pixdim"][1 : ndim + 1]
+
+    origin = np.zeros((ndim))
+    origin[:3] = q_form[:3, 3]
+
+    direction = np.diag(np.ones(ndim))
+    direction[:3, :3] = q_form[:3, :3] / spacing[:3]
+
+    ants_img = ants.from_numpy(
+        data=img.get_data().astype(np.float),
+        origin=origin.tolist(),
+        spacing=spacing.tolist(),
+        direction=direction,
+    )
+
+    return ants_img
