f93d72392779c6abedcb4d08fcb59e89b339529c
stnava <stnava@gmail.com>
2019-10-07 11:37:26 -0400

    STYLE: back to antspyx

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ class CMakeBuild(build_ext):
 long_description = open("README.md").read()
 
 setup(
-    name="antspy",
+    name="antspyx",
     version=version,
     author="Brian B. Avants and Nicholas Cullen",
     author_email="stnava@gmail.com",
