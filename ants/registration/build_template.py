85171b6027472230c77e24ade95767fb8ad2f72e
stnava <stnava@gmail.com>
2020-04-10 10:28:40 -0400

    ENH: weighted template building

--- ants/registration/build_template.py
+++ ants/registration/build_template.py
@@ -1,6 +1,6 @@
+__all__ = ["build_template"]
 
-
-__all__ = ['build_template']
+import numpy as np
 
 from tempfile import mktemp
 
@@ -8,14 +8,18 @@ from .reflect_image import reflect_image
 from .interface import registration
 from .apply_transforms import apply_transforms
 from ..core import ants_image_io as iio
+from ..utils import iMath as utils
 
 
 def build_template(
     initial_template=None,
     image_list=None,
-    iterations = 3,
-    gradient_step = 0.2,
-    **kwargs ):
+    iterations=3,
+    gradient_step=0.2,
+    blending_weight=0.9,
+    weights=None,
+    **kwargs
+):
     """
     Estimate an optimal template from an input image_list
 
@@ -35,6 +39,12 @@ def build_template(
     gradient_step : scalar
         for shape update gradient
 
+    blending_weight : scalar
+        weight for image blending
+
+    weights : vector
+        weight for each input image
+
     kwargs : keyword args
         extra arguments passed to ants registration
 
@@ -45,38 +55,46 @@ def build_template(
     Example
     -------
     >>> import ants
-    >>> image = ants.image_read( ants.get_ants_data('r16') , 'float')
-    >>> image2 = ants.image_read( ants.get_ants_data('r27') , 'float')
-    >>> image3 = ants.image_read( ants.get_ants_data('r85') , 'float')
+    >>> image = ants.image_read( ants.get_ants_data('r16') )
+    >>> image2 = ants.image_read( ants.get_ants_data('r27') )
+    >>> image3 = ants.image_read( ants.get_ants_data('r85') )
     >>> timage = ants.build_template( image_list = ( image, image2, image3 ) )
+    >>> timagew = ants.build_template( image_list = ( image, image2, image3 ), weights = (5,1,1) )
     """
-    if 'type_of_transform' not in kwargs:
-        type_of_transform = 'SyN'
+    if "type_of_transform" not in kwargs:
+        type_of_transform = "SyN"
     else:
-        type_of_transform = kwargs.pop('type_of_transform')
+        type_of_transform = kwargs.pop("type_of_transform")
 
-    wt = 1.0 / len( image_list )
+    if weights is None:
+        weights = np.repeat(1.0 / len(image_list), len(image_list))
+    weights = [x / sum(weights) for x in weights]
     if initial_template is None:
-        initial_template = image_list[ 0 ] * 0
-        for i in range( len( image_list ) ):
-            initial_template = initial_template + image_list[ i ] * wt
+        initial_template = image_list[0] * 0
+        for i in range(len(image_list)):
+            initial_template = initial_template + image_list[i] * weights[i]
 
     xavg = initial_template.clone()
-    for i in range( iterations ):
-        for k in range( len( image_list ) ):
-            w1 = registration( xavg, image_list[k],
-              type_of_transform=type_of_transform, **kwargs )
+    for i in range(iterations):
+        for k in range(len(image_list)):
+            w1 = registration(
+                xavg, image_list[k], type_of_transform=type_of_transform, **kwargs
+            )
             if k == 0:
-              wavg = iio.image_read( w1['fwdtransforms'][0] ) * wt
-              xavgNew = w1['warpedmovout'] * wt
+                wavg = iio.image_read(w1["fwdtransforms"][0]) * weights[k]
+                xavgNew = w1["warpedmovout"] * weights[k]
             else:
-              wavg = wavg + iio.image_read( w1['fwdtransforms'][0] ) * wt
-              xavgNew = xavgNew + w1['warpedmovout'] * wt
-        print( wavg.abs().mean() )
+                wavg = wavg + iio.image_read(w1["fwdtransforms"][0]) * weights[k]
+                xavgNew = xavgNew + w1["warpedmovout"] * weights[k]
+        print(wavg.abs().mean())
         wscl = (-1.0) * gradient_step
         wavg = wavg * wscl
-        wavgfn = mktemp(suffix='.nii.gz')
+        wavgfn = mktemp(suffix=".nii.gz")
         iio.image_write(wavg, wavgfn)
-        xavg = apply_transforms( xavgNew, xavgNew, wavgfn )
+        xavg = apply_transforms(xavgNew, xavgNew, wavgfn)
+        if blending_weight is not None:
+            xavg = xavg * blending_weight + utils.iMath(xavg, "Sharpen") * (
+                1.0 - blending_weight
+            )
 
     return xavg
