e0c3573a512f83b5cfb09e0f4ab6e741d1308a4d
Nick Tustison <ntustison@gmail.com>
2020-05-02 14:09:56 -0700

    COMP:  Update ANTs tag for sim field.

--- scripts/configure_ANTsPy.sh
+++ scripts/configure_ANTsPy.sh
@@ -23,7 +23,7 @@ cp data/* ~/.antspy/
 # clone ANTs and move all files into library directory
 
 antsgit=https://github.com/ANTsX/ANTs.git
-antstag=4658cde31d91ac9621d351becfcb26ca3b609151 # unsharp mask
+antstag=8c61a430099e9d5ba580b7255132cfa58a7ee880 # sim. displacement field
 echo "ANTS;${antstag}" >> ./data/softwareVersions.csv
 
 cd ants/lib # go to lib dir
