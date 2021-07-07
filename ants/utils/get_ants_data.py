021087192635a56c8c371928ed67ebb3155f6a52
Nick Tustison <ntustison@gmail.com>
2021-07-06 20:18:17 -0700

    ENH:  Refactor get_ants_data

--- ants/utils/get_ants_data.py
+++ ants/utils/get_ants_data.py
@@ -6,12 +6,12 @@ __all__ = ['get_ants_data',
            'get_data']
 
 import os
+import requests
+import tempfile
 
-DATA_PATH = os.path.expanduser('~/.antspy/')
-
-def get_data(name=None):
+def get_data(file_id=None, target_file_name=None, antsx_cache_directory=None):
     """
-    Get ANTsPy test data filename
+    Get ANTsPy test data file
 
     ANTsR function: `getANTsRData`
 
@@ -22,6 +22,8 @@ def get_data(name=None):
         Options:
             - 'r16'
             - 'r27'
+            - 'r30'
+            - 'r62'
             - 'r64'
             - 'r85'
             - 'ch2'
@@ -37,23 +39,68 @@ def get_data(name=None):
     >>> import ants
     >>> mnipath = ants.get_ants_data('mni')
     """
-    os.makedirs(DATA_PATH, exist_ok=True)
 
-    if name is None:
-        files = []
-        for fname in os.listdir(DATA_PATH):
-            if (fname.endswith('.nii.gz')) or (fname.endswith('.jpg') or (fname.endswith('.csv'))):
-                fname = os.path.join(DATA_PATH, fname)
-                files.append(fname)
-        return files
+    def switch_data(argument):
+        switcher = {
+            "r16": "https://ndownloader.figshare.com/files/28726512",
+            "r27": "https://ndownloader.figshare.com/files/28726515",
+            "r30": "https://ndownloader.figshare.com/files/28726518",
+            "r62": "https://ndownloader.figshare.com/files/28726521",
+            "r64": "https://ndownloader.figshare.com/files/28726524",
+            "r85": "https://ndownloader.figshare.com/files/28726527",
+            "ch2": "https://ndownloader.figshare.com/files/28726494",
+            "mni": "https://ndownloader.figshare.com/files/28726500",
+            "surf": "https://ndownloader.figshare.com/files/28726530"
+        }
+        return(switcher.get(argument, "Invalid argument."))
 
-    datapath = None
-    for fname in os.listdir(DATA_PATH):
-        if (name == fname.split('.')[0]) or ((name+'slice') == fname.split('.')[0]):
-            datapath = os.path.join(DATA_PATH, fname)
+    if antsx_cache_directory is None:
+        antsx_cache_directory = os.path.expanduser('~/.antspy/')
+    os.makedirs(antsx_cache_directory, exist_ok=True)
 
-    if datapath is None:
-        raise ValueError('File doesnt exist. Options: ' , os.listdir(DATA_PATH))
-    return datapath
+    if os.path.isdir(antsx_cache_directory) == False:
+        antsx_cache_directory = tempfile.TemporaryDirectory()
+
+    if file_id == None:
+        raise ValueError("Missing file id.")
+
+    valid_list = ("r16",
+                  "r27",
+                  "r64",
+                  "r85",
+                  "ch2",
+                  "mni",
+                  "surf"
+                  "show")
+
+    if not file_id in valid_list:
+        raise ValueError("No data with the id you passed - try \"show\" to get list of valid ids.")
+
+    if file_id == "show":
+       return(valid_list)
+
+    url = switch_data(file_id)
+
+    if target_file_name == None:
+        extension = ".jpg"
+        if file_id == "ch2" or file_id == "mni" or file_id == "surf":
+            extension = ".nii.gz"
+        if extension == ".jpg":
+            target_file_name = antsx_cache_directory + file_id + "slice" + extension
+        else:
+            target_file_name = antsx_cache_directory + file_id + extension
+
+    target_file_name_path = target_file_name
+    if target_file_name == None:
+        target_file = tempfile.NamedTemporaryFile(prefix=target_file_name, dir=antsx_cache_directory)
+        target_file_name_path = target_file.name
+        target_file.close()
+
+    if not os.path.exists(target_file_name_path):
+        r = requests.get(url)
+        with open(target_file_name_path, 'wb') as f:
+            f.write(r.content)
+
+    return(target_file_name_path)
 
 get_ants_data = get_data
