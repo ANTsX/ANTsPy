59e940535e4c581e0e84fefbdd042fc70fc4f6ad
Nick Tustison <ntustison@gmail.com>
2023-02-22 20:41:00 -0800

    WIP:  Add time-varying point set transform fitting.

--- ants/registration/landmark_transforms.py
+++ ants/registration/landmark_transforms.py
@@ -12,6 +12,22 @@ from ..utils import smooth_image
 from ..utils import compose_displacement_fields
 from ..utils import invert_displacement_field
 
+def convergence_monitoring(values, window_size=10):
+     if len(values) >= window_size:
+         u = np.linspace(0.0, 1.0, num=window_size)
+         scattered_data = np.expand_dims(values[-window_size:], axis=-1)
+         parametric_data = np.expand_dims(u, axis=-1)
+         spacing = 1 / (window_size-1)
+         bspline_line = fit_bspline_object_to_scattered_data(scattered_data, parametric_data,
+             parametric_domain_origin=[0.0], parametric_domain_spacing=[spacing],
+             parametric_domain_size=[window_size], number_of_fitting_levels=1, mesh_size=1,
+             spline_order=1)
+         bspline_slope = -(bspline_line[1][0] - bspline_line[0][0]) / spacing
+         return(bspline_slope)
+     else:
+         return None
+
+
 def fit_transform_to_paired_points( moving_points,
                                     fixed_points,
                                     transform_type="affine",
@@ -31,7 +47,7 @@ def fit_transform_to_paired_points( moving_points,
                                     verbose=False
                                    ):
     """
-    Estimate an optimal matrix transformation from paired points, potentially landmarks
+    Estimate a transform from corresponding fixed and moving landmarks.
 
     ANTsR function: fitTransformToPairedPoints
 
@@ -52,23 +68,23 @@ def fit_transform_to_paired_points( moving_points,
         ridge penalty in [0,1] for linear transforms.
 
     domain_image : ANTs image
-        defines physical domain of the B-spline transform.
+        defines physical domain of the nonlinear transforms.
 
     number_of_fitting_levels : integer
-        integer specifying the number of fitting levels (B-spline only).
+        integer specifying the number of fitting levels.
 
     mesh_size : integer or array
-        defining the mesh size at the initial fitting level (B-spline only).
+        defining the mesh size at the initial fitting level.
 
     spline_order : integer
-        spline order of the B-spline object. (B-spline only).
+        spline order of the B-spline object.
 
     enforce_stationary_boundary : boolean
         ensure no displacements on the image boundary (B-spline only).
 
     displacement_weights : array
-        defines the individual weighting of the corresponding scattered data value.  (B-spline
-        only).  Default = NULL meaning all displacements are weighted the same.
+        defines the individual weighting of the corresponding scattered data value.
+        Default = NULL meaning all displacements are weighted the same.
 
     number_of_compositions : integer
         total number of compositions for the diffeomorphic transforms.
@@ -139,21 +155,6 @@ def fit_transform_to_paired_points( moving_points,
          field = iio2.from_numpy(field_array, origin=origin, spacing=spacing, direction=direction,
                  has_components=True)
          return(field)
-
-    def convergence_monitoring(values, window_size=10):
-         if len(values) >= window_size:
-             u = np.linspace(0.0, 1.0, num=window_size)
-             scattered_data = np.expand_dims(values[-window_size:], axis=-1)
-             parametric_data = np.expand_dims(u, axis=-1)
-             spacing = 1 / (window_size-1)
-             bspline_line = fit_bspline_object_to_scattered_data(scattered_data, parametric_data,
-                 parametric_domain_origin=[0.0], parametric_domain_spacing=[spacing],
-                 parametric_domain_size=[window_size], number_of_fitting_levels=1, mesh_size=1,
-                 spline_order=1)
-             bspline_slope = -(bspline_line[1][0] - bspline_line[0][0]) / spacing
-             return(bspline_slope)
-         else:
-             return None
 
     allowed_transforms = ['rigid', 'affine', 'similarity', 'bspline', 'diffeo', 'syn', 'tv', 'time-varying']
     if not transform_type.lower() in allowed_transforms:
@@ -507,6 +508,281 @@ def fit_transform_to_paired_points( moving_points,
 
     else:
         raise ValueError("Unrecognized transform_type.")
+
+
+def fit_time_varying_transform_to_point_sets(point_sets,
+                                             time_points=None,
+                                             number_of_integration_points=None,
+                                             domain_image = None,
+                                             number_of_fitting_levels=4,
+                                             mesh_size=1,
+                                             spline_order=3,
+                                             displacement_weights=None,
+                                             number_of_compositions=10,
+                                             composition_step_size=0.5,
+                                             sigma=0.0,
+                                             convergence_threshold=0.0,
+                                             rasterize_points=False,
+                                             verbose=False
+                                            ):
+    """
+
+    Estimate a transform from corresponding point sets (> 2).
+
+    ANTsR function: fitTimeVaryingTransform
+
+    Arguments
+    ---------
+    point_sets : list of arrays
+        Corresponding points across sets specified in physical space as a n x d matrix where n
+        is the number of points and \code{d} is the dimensionality.
+
+    time_points : array of ordered scalars between 0 and 1
+        Set of scalar values, one for each point-set designating its time position in the velocity
+        flow.  If not set, it defaults to equal spacing between 0 and 1.
+
+    number_of_integration_points : integer
+        Time-varying velocity field parameter.  Needs to be equal to or greater than the number of
+        point sets.  If not specified, it defaults to the number of point sets.
+
+    domain_image : ANTs image
+        defines physical domain of the nonlinear transform.
+
+    number_of_fitting_levels : integer
+        integer specifying the number of fitting levels.
+
+    mesh_size : integer or array
+        defining the mesh size at the initial fitting level.
+
+    spline_order : integer
+        spline order of the B-spline object.
+
+    displacement_weights : array
+        defines the individual weighting of the corresponding scattered data value.  Default = NULL
+        meaning all displacements are weighted the same.
+
+    number_of_compositions : integer
+        total number of compositions.
+
+    composition_step_size : scalar
+        scalar multiplication factor.
+
+    sigma : scalar
+        Gaussian smoothing sigma (in mm).
+
+    convergence_threshold : scalar
+        Composition-based convergence parameter using a window size of 10 values.
+
+    rasterize_points : boolean
+       Use nearest neighbor rasterization of points for estimating the update
+       field (potential speed-up).  Default = False.
+
+    verbose : bool
+        Print progress to the screen.
+
+    Returns
+    -------
+
+    ANTs transform
+
+    Example
+    -------
+    >>> import ants
+    >>> import numpy as np
+    """
+
+    def create_zero_velocity_field(domain_image, number_of_time_points=2):
+         field_array = np.zeros((*domain_image.shape, number_of_time_points, domain_image.dimension))
+         origin = (*domain_image.origin, 0.0)
+         spacing = (*domain_image.spacing, 1.0)
+         direction = np.eye(domain_image.dimension + 1)
+         direction[0:domain_image.dimension,0:domain_image.dimension] = domain_image.direction
+         field = iio2.from_numpy(field_array, origin=origin, spacing=spacing, direction=direction,
+                 has_components=True)
+         return(field)
+
+    if not isinstance(point_sets, list):
+        raise ValueError("point_sets should be a list of corresponding point sets.")
+
+    number_of_point_sets = len(point_sets)
+
+    if time_points is not None and len(time_points) != number_of_point_sets:
+        raise ValueError("The number of time points should be the same as the number of point sets.")
+
+    if time_points is None:
+        time_points = np.linspace(0.0, 1.0, number_of_point_sets)
+    time_points = np.array(time_points)
+
+    if np.any(time_points < 0.0) or np.any(time_points > 1.0):
+        raise ValueError("time point values should be between 0 and 1.")
+
+    if number_of_integration_points is None:
+        number_of_integration_points = len(time_points)
+
+    if number_of_integration_points < number_of_point_sets:
+        raise ValueError("The number of integration points should be at least as great as the number of point sets.")
+
+    if number_of_point_sets < 3:
+        raise ValueError("Expecting three or greater point sets.")
+
+    number_of_points = point_sets[0].shape[0]
+    dimensionality = point_sets[0].shape[1]
+    for i in range(1, number_of_point_sets):
+        if point_sets[i].shape[0] != number_of_points:
+            raise ValueError("Point sets should match in terms of the number of points.")
+        if point_sets[i].shape[1] != dimensionality:
+            raise ValueError("Point sets should match in terms of dimensionality.")
+
+    if verbose:
+        start_total_time = time.time()
+
+    updated_fixed_points = np.zeros(point_sets[0].shape)
+    updated_moving_points = np.zeros(point_sets[0].shape)
+
+    velocity_field = create_zero_velocity_field(domain_image, number_of_integration_points)
+    velocity_field_array = velocity_field.numpy()
+
+    last_update_derivative_field = create_zero_velocity_field(domain_image, number_of_integration_points)
+    last_update_derivative_field_array = last_update_derivative_field.numpy()
+
+    error_values = []
+    for i in range(number_of_compositions):
+
+        if verbose:
+            start_time = time.time()
+
+        update_derivative_field = create_zero_velocity_field(domain_image, number_of_integration_points)
+        update_derivative_field_array = update_derivative_field.numpy()
+
+        for n in range(number_of_integration_points):
+
+            t = n / (number_of_integration_points - 1.0)
+
+            t_index = 0
+            for j in range(1, number_of_point_sets):
+                if time_points[j-1] <= t and time_points[j] >= t:
+                    t_index = j
+                    break
+
+            if n > 0 and n < number_of_integration_points - 1 and time_points[t_index-1] == t:
+                updated_fixed_points[:] = point_sets[t_index-1]
+                integrated_inverse_field = integrate_velocity_field(velocity_field, time_points[t_index], t, 100)
+                integrated_inverse_field_xfrm = txio.transform_from_displacement_field(integrated_inverse_field)
+                for j in range(updated_moving_points.shape[0]):
+                    updated_moving_points[j,:] = integrated_inverse_field_xfrm.apply_to_point(tuple(point_sets[t_index][j,:]))
+
+                update_derivative_field_at_timepoint_forward = fit_bspline_displacement_field(
+                  displacement_origins=updated_fixed_points,
+                  displacements=updated_moving_points - updated_fixed_points,
+                  displacement_weights=displacement_weights,
+                  origin=domain_image.origin,
+                  spacing=domain_image.spacing,
+                  size=domain_image.shape,
+                  direction=domain_image.direction,
+                  number_of_fitting_levels=number_of_fitting_levels,
+                  mesh_size=mesh_size,
+                  spline_order=spline_order,
+                  enforce_stationary_boundary=True,
+                  rasterize_points=rasterize_points
+                  )
+
+                updated_moving_points[:] = point_sets[t_index-1]
+                integrated_forward_field = integrate_velocity_field(velocity_field, time_points[t_index-2], t, 100)
+                integrated_forward_field_xfrm = txio.transform_from_displacement_field(integrated_forward_field)
+                for j in range(updated_fixed_points.shape[0]):
+                    updated_fixed_points[j,:] = integrated_forward_field_xfrm.apply_to_point(tuple(point_sets[t_index-2][j,:]))
+
+                update_derivative_field_at_timepoint_back = fit_bspline_displacement_field(
+                  displacement_origins=updated_fixed_points,
+                  displacements=updated_moving_points - updated_fixed_points,
+                  displacement_weights=displacement_weights,
+                  origin=domain_image.origin,
+                  spacing=domain_image.spacing,
+                  size=domain_image.shape,
+                  direction=domain_image.direction,
+                  number_of_fitting_levels=number_of_fitting_levels,
+                  mesh_size=mesh_size,
+                  spline_order=spline_order,
+                  enforce_stationary_boundary=True,
+                  rasterize_points=rasterize_points
+                  )
+
+                update_derivative_field_at_timepoint <- ( update_derivative_field_at_timepoint_forward +
+                                                          update_derivative_field_at_timepoint_back ) / 2.0
+
+            else:
+                if t == 0.0 and time_points[t_index-1] == 0.0:
+                    updated_fixed_points[:] = point_sets[0]
+                else:
+                    integrated_forward_field = integrate_velocity_field(velocity_field, time_points[t_index-1], t, 100)
+                    integrated_forward_field_xfrm = txio.transform_from_displacement_field(integrated_forward_field)
+                    for j in range(updated_fixed_points.shape[0]):
+                        updated_fixed_points[j,:] = integrated_forward_field_xfrm.apply_to_point(tuple(point_sets[t_index-1][j,:]))
+
+                if t == 1.0 and time_points[t_index] == 1.0:
+                    updated_moving_points[:] = point_sets[-1]
+                else:
+                    integrated_inverse_field = integrate_velocity_field(velocity_field, time_points[t_index], t, 100)
+                    integrated_inverse_field_xfrm = txio.transform_from_displacement_field(integrated_inverse_field)
+                    for j in range(updated_moving_points.shape[0]):
+                        updated_moving_points[j,:] = integrated_inverse_field_xfrm.apply_to_point(tuple(point_sets[t_index][j,:]))
+
+                update_derivative_field_at_timepoint = fit_bspline_displacement_field(
+                  displacement_origins=updated_fixed_points,
+                  displacements=updated_moving_points - updated_fixed_points,
+                  displacement_weights=displacement_weights,
+                  origin=domain_image.origin,
+                  spacing=domain_image.spacing,
+                  size=domain_image.shape,
+                  direction=domain_image.direction,
+                  number_of_fitting_levels=number_of_fitting_levels,
+                  mesh_size=mesh_size,
+                  spline_order=spline_order,
+                  enforce_stationary_boundary=True,
+                  rasterize_points=rasterize_points
+                  )
+
+                if sigma > 0:
+                    update_derivative_field_at_timepoint = smooth_image(update_derivative_field_at_timepoint, sigma)
+
+                update_derivative_field_at_timepoint_array = update_derivative_field_at_timepoint.numpy()
+                max_norm = np.sqrt(np.amax(np.sum(np.square(update_derivative_field_at_timepoint_array), axis=-1, keepdims=False)))
+                update_derivative_field_at_timepoint_array /= max_norm
+                if domain_image.dimension == 2:
+                    update_derivative_field_array[:,:,n,:] = update_derivative_field_at_timepoint_array
+                elif domain_image.dimension == 3:
+                    update_derivative_field_array[:,:,:,n,:] = update_derivative_field_at_timepoint_array
+
+        update_derivative_field_array = (update_derivative_field_array + last_update_derivative_field_array) * 0.5
+        last_update_derivative_field = update_derivative_field_array
+
+        velocity_field_array += (update_derivative_field_array * composition_step_size)
+        velocity_field = iio2.from_numpy(velocity_field_array, origin=velocity_field.origin,
+                                         spacing=velocity_field.spacing, direction=velocity_field.direction,
+                                         has_components=True)
+
+        error_values.append(np.mean(np.sqrt(np.sum(np.square(updated_moving_points - updated_fixed_points), axis=1, keepdims=True))))
+        convergence_value = convergence_monitoring(error_values)
+        if verbose:
+            end_time = time.time()
+            diff_time = end_time - start_time
+            print("Composition " + str(i) + ": error = " + str(error_values[-1]) +
+                  " (convergence = " + str(convergence_value) + ", elapsed time = " + str(diff_time) + ")")
+        if not convergence_value is None and convergence_value < convergence_threshold:
+            break
+
+    forward_xfrm = txio.transform_from_displacement_field(integrate_velocity_field(velocity_field, 0.0, 1.0, 100))
+    inverse_xfrm = txio.transform_from_displacement_field(integrate_velocity_field(velocity_field, 1.0, 0.0, 100))
+
+    if verbose:
+        end_total_time = time.time()
+        diff_total_time = end_total_time - start_total_time
+        print("Total elapsed time = " + str(diff_total_time) + ".")
+
+    return_dict = {'forward_transform': forward_xfrm,
+                   'inverse_transform': inverse_xfrm,
+                   'velocity_field': velocity_field}
+    return(return_dict)
 
 
 
