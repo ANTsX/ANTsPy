b67c22f5896f43d3ffadbea763e57d083cc6174c
Philip Cook <cookpa@pennmedicine.upenn.edu>
2023-06-08 10:37:20 -0400

    ENH: Rewrite Windows tests (#469)
    Use Powershell instead of cmd script, exit with error code on test failures.

---
+++ tests/run_tests.ps1
@@ -0,0 +1,32 @@
+$PYCMD = "python"
+
+$PYTHON_SCRIPTS = @(
+    "test_core_ants_image.py",
+    "test_core_ants_image_io.py",
+    "test_core_ants_transform.py",
+    "test_core_ants_transform_io.py",
+    "test_core_ants_metric.py",
+    "test_learn.py",
+    "test_registation.py",
+    "test_segmentation.py",
+    "test_utils.py",
+    "test_bugs.py"
+)
+
+$scriptFailed = $false
+
+Set-Location -Path ".\tests"
+
+foreach ($script in $PYTHON_SCRIPTS) {
+    Write-Host "Running Python script $script"
+    & $PYCMD $script $args
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Python script $script failed with exit code $LASTEXITCODE"
+        $scriptFailed = $true
+    }
+}
+
+if ($scriptFailed) {
+    exit 1
+}
