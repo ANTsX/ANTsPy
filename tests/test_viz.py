f5a45f47a93aaac4a2a43e2fde0845861ad43eab
ncullen93 <nicholas.cullen@med.lu.se>
2024-02-21 21:02:44 +0100

    test matplotlib plot only

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -33,26 +33,5 @@ class TestModule_plot(unittest.TestCase):
             ants.plot(img, filename=filename)
 
 
-class TestModule_create_tiled_mosaic(unittest.TestCase):
-
-    def setUp(self):
-        img2d = ants.image_read(ants.get_ants_data('r16'))
-        img3d = ants.image_read(ants.get_ants_data('mni'))
-        self.imgs = [img2d, img3d]
-
-    def tearDown(self):
-        pass
-
-    def test_example(self):
-        img = ants.image_read(ants.get_ants_data('ch2')).resample_image((3,3,3))
-
-        # test with output
-        outfile = mktemp(suffix='.png')
-        p = ants.create_tiled_mosaic(img, output=outfile)
-
-        # rgb is not none
-        rgb = img.clone()
-        p = ants.create_tiled_mosaic(img, rgb=rgb, output=outfile)
-
 if __name__ == '__main__':
     run_tests()
