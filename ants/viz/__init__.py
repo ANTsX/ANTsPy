01830acef7f9313c52526e6c70acdb0a91617a48
ncullen93 <nicholas.cullen@med.lu.se>
2024-03-12 21:30:17 +0100

    remove redundant plotting fn (see plot_orth_stack)

--- ants/viz/__init__.py
+++ ants/viz/__init__.py
@@ -5,7 +5,6 @@ from .movie import movie
 from .plot_hist import plot_hist
 from .plot_grid import plot_grid
 from .plot_ortho import plot_ortho
-from .plot_ortho_double import plot_ortho_double
 from .plot_ortho_stack import plot_ortho_stack
 from .plot_directory import plot_directory
 
