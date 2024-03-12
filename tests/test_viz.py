d038bf9fcdf5f2da274884db824dbcb228a568fa
ncullen93 <nicholas.cullen@med.lu.se>
2024-03-12 21:47:47 +0100

    add viz tests

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -32,6 +32,31 @@ class TestModule_plot(unittest.TestCase):
         for img in self.imgs:
             ants.plot(img, filename=filename)
 
+class TestModule_plot_ortho(unittest.TestCase):
+
+    def setUp(self):
+        img3d = ants.image_read(ants.get_ants_data('mni'))
+        self.imgs = [img3d]
+
+    def tearDown(self):
+        pass
+
+    def test_plot_example(self):
+        filename = mktemp(suffix='.png')
+        for img in self.imgs:
+            ants.plot_ortho(img, filename=filename)
+
+class TestModule_plot_ortho_stack(unittest.TestCase):
+
+    def setUp(self):
+        self.img = ants.image_read(ants.get_ants_data('mni'))
+
+    def tearDown(self):
+        pass
+
+    def test_plot_example(self):
+        filename = mktemp(suffix='.png')
+        ants.plot_ortho_stack([self.img, self.img], filename=filename)
 
 if __name__ == '__main__':
     run_tests()
