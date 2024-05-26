b8a6701350e60ae335c1b1a3b6cd5008e29bc02a
ncullen93 <nicholas.cullen@med.lu.se>
2024-05-26 15:07:53 +0200

    add back old float64 cast

--- ants/core/ants_image_io.py
+++ ants/core/ants_image_io.py
@@ -89,12 +89,17 @@ def from_numpy(
     ANTsImage
         image with given data and any given information
     """
+    
+    # this is historic but should be removed once tests can pass without it
+    if str(data.dtype) == 'float64':
+        data = data.astype('float32')
+    
     # if dtype is not supported, cast to best available
     best_dtype = infer_dtype(data.dtype)
     if best_dtype != data.dtype:
         warnings.warn(f'Warning: The dtype {data.dtype} is not supported so the array will be casted to {best_dtype}. Cast it manually before calling `ants.from_numpy()` to remove this warning.')
         data = data.astype(best_dtype)
-        
+    
     img = _from_numpy(data.T.copy(), origin, spacing, direction, has_components, is_rgb)
     return img
 
