e114676c64b6613cb067de8e74a55c12e6c6f0e9
stnava <stnava@gmail.com>
2024-11-27 15:21:29 -0500

    PERF: fix type compatibility and style with accessing pandas dataframe

--- ants/label/label_overlap_measures.py
+++ ants/label/label_overlap_measures.py
@@ -40,6 +40,5 @@ def label_overlap_measures(source_image, target_image):
     libfn = get_lib_fn("labelOverlapMeasures%iD" % source_image_int.dimension)
     df = libfn(source_image_int.pointer, target_image_int.pointer)
     df = pd.DataFrame(df)
-    with pd.option_context('mode.chained_assignment', None):
-        df.Label[0] = "All"
+    df.loc[0, 'Label'] = 'All'.astype(object)
     return df
