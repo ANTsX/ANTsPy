762409175e0ac251bc50e718cc3c132015b9de7a
Philip Cook <cookpa@pennmedicine.upenn.edu>
2025-12-23 10:20:43 -0500

    BUG: Remove deprecated numpy calls to reshape

--- ants/deeplearn/randomly_transform_image_data.py
+++ ants/deeplearn/randomly_transform_image_data.py
@@ -48,7 +48,7 @@ def randomly_transform_image_data(reference_image,
         Number of simulated output image sets.
 
     transform_type : string
-        One of the following options: "translation", "rotation", "rigid", "scaleShear", 
+        One of the following options: "translation", "rotation", "rigid", "scaleShear",
         "affine", "deformation", "affineAndDeformation".
 
     sd_affine : float
@@ -113,13 +113,13 @@ def randomly_transform_image_data(reference_image,
 
         if transform_type == 'translation':
             random_epsilon[:(len(identity_parameters) - image.dimension)] = 0
-        elif transform_type == "rotation": 
+        elif transform_type == "rotation":
             random_epsilon[(len(identity_parameters) - image.dimension):] = 0
 
         random_parameters = identity_parameters + random_epsilon
         random_matrix = np.reshape(
             random_parameters[:(len(identity_parameters) - image.dimension)],
-            newshape=(image.dimension, image.dimension))
+            shape=(image.dimension, image.dimension))
         decomposition = ants.polar_decomposition(random_matrix)
 
         if transform_type == "rotation" or transform_type == "rigid":
@@ -130,7 +130,7 @@ def randomly_transform_image_data(reference_image,
             random_matrix = decomposition['P']
 
         random_parameters[:(len(identity_parameters) - image.dimension)] = \
-            np.reshape(random_matrix, newshape=(len(identity_parameters) - image.dimension))
+            np.reshape(random_matrix, shape=(len(identity_parameters) - image.dimension))
         ants.set_ants_transform_parameters(transform, random_parameters)
         return(transform)
 
